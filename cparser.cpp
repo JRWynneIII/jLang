a14f8a4de89bbe6cc033f12a5c5f90580ee8a097
James Riley Wynne III <wynnejr@ornl.gov>
2014-12-08 13:17:24 -0500

    Fixed cparser. Combined all variable tables into one by use of a struct

--- cparser.cpp
+++ cparser.cpp
@@ -8,86 +8,90 @@
 #include<string.h>
 using namespace std;
 extern FILE* yyin;
+
 typedef struct symEntry
 {
-  string value;       //Holds the value of the var in a string format. Use atof() or atoi() to convert
   string type;        //Holds string telling what type the value is
-  char scope;         //Tells scope of var. 'g': global 'f': function 'l': loop
+
+  char* valC = NULL;
+  int valI;
+  double valD;
+  string valS;
+
+  char scope = 'g';         //Tells scope of var. 'g': global 'f': function 'l': loop
   string originFunc;  //Provides context for scope
 } tblEntry;
 
 map<string,tblEntry> varTbl;
 
-map<string,string> stringTable;
-map<string,double> doubleTable;
-map<string,int> intTable;
-vector<string> args;
-vector<string> funcNames;
-map<string,vector<string> > funcTable;
-
-
-void checkDupVar(string name, string type)
+bool checkVar(string name)
 {
-  if(strcmp(name.c_str(),"string"))
+  map<string,tblEntry>::iterator it;
+  for(it = varTbl.begin(); it != varTbl.end(); it++)
   {
-    map<string,string>::iterator it;
-    for (it = stringTable.begin(); it != stringTable.end(); it++)
-      if(it->first == name)
-      {
-        cout<<"ERROR: Duplicate Variable " << name << endl;
-        exit(EXIT_FAILURE);
-      }
+    if(it->first == name)
+      return true;
   }
-  else if (strcmp(name.c_str(),"double"))
-  {
-    map<string,double>::iterator it;
-    for (it = doubleTable.begin(); it != doubleTable.end(); it++)
-      if(it->first == name)
-      {
-        cout<<"ERROR: Duplicate Variable " << name << endl;
-        exit(EXIT_FAILURE);
-      }
-  }
-  else if (strcmp(name.c_str(),"int"))
-  {
-    map<string,int>::iterator it;
-    for (it = intTable.begin(); it != intTable.end(); it++)
-      if(it->first == name)
-      {
-        cout<<"ERROR: Duplicate Variable " << name << endl;
-        exit(EXIT_FAILURE);
-      }
-  }
-  else if (strcmp(name.c_str(), "func"))
-  {
-    vector<string>::iterator it;
-    for(it = funcNames.begin(); it!= funcNames.end(); it++)
-    {
-      if (*it == name)
-      {
-        cout <<"ERROR: Duplicate Function Definition: " << name << endl;
-        exit(EXIT_FAILURE);
-      } 
-    }
-  }  
+  return false;
 }
 
-void storeVar(string name, string type, string value)
+void storeNewVar(string name, string type, string value)
 {
-  if(strcmp(name.c_str(),"string"))
-     stringTable[name] = value;
-  else if (strcmp(name.c_str(),"double"))
-     doubleTable[name] = atof(value.c_str());
-  else if (strcmp(name.c_str(),"int"))
-     intTable[name] = atoi(value.c_str());
-  else if (strcmp(name.c_str(), "func"))
+  if (checkVar(name))
   {
-     funcTable[name] = args;
-     args.clear();
+    cout << "ERROR: Duplicate variable defined! " << name << endl;
+    exit(EXIT_FAILURE);
   }
+  if (strcmp(type.c_str(),"char") == 0)
+  {
+    tblEntry var;
+    var.type = type;
+    var.valC = const_cast<char *>(value.c_str());
+    varTbl[name] = var;
+  }
+  else if (strcmp(type.c_str(),"int") == 0)
+  {
+    tblEntry var;
+    var.type = type;
+    var.valI = atoi(value.c_str());
+    varTbl[name] = var;
+  }
+  else if (strcmp(type.c_str(),"double") == 0)
+  {
+    tblEntry var;
+    var.type = type;
+    var.valD = atof(value.c_str());
+    varTbl[name] = var;
+  }
+  else if (strcmp(type.c_str(),"string") == 0)
+  {
+    tblEntry var;
+    var.type = type;
+    var.valS = value;
+    varTbl[name] = var;
+  }
+}
+
+void updateVar(string name, string value)
+{
+  if (checkVar(name))
+  {
+    cout << "ERROR: '" << name << "' not defined!\n";
+    exit(EXIT_FAILURE);
+  }
+  tblEntry var = varTbl[name];
+  string type = var.type;
+  if (type == "int")
+    var.valI = atoi(value.c_str());
+  else if (type == "double")
+    var.valD = atof(value.c_str());
+  else if (type == "char")
+    var.valC = const_cast<char *>(value.c_str());
+  else if (type == "string")
+    var.valS = value;
 }
 
 int parseStart()
 {
-
+  
 }
