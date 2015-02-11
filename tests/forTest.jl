d3cd3c266a38872e6c1ee4f75e58dea304801106
James Riley Wynne III <wynnejr@ornl.gov>
2015-02-11 01:19:33 -0500

    Fixed bug in compile script. Expanded for loop testprogram

--- tests/forTest.jl
+++ tests/forTest.jl
@@ -6,15 +6,18 @@ func main() -> int {
   int i = 0
   int j = 0
   char a = 'z'
+  char newLine = '\n'
   for i=0,10 {
     a = 'a'
     putchar(a)
   }
+  putchar(newLine)
   for i=0,10 {
     for j=0,10 {
       a = 'z'
       putchar(a)
     }
+    putchar(newLine)
   }
   1
 }
