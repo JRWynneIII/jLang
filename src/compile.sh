f66b91ec59c66fcca8d53d801d44ad95f72df5eb
James Riley Wynne III <wynnejr@ornl.gov>
2015-03-16 21:54:03 -0400

    Fixed bug with linking

--- src/compile.sh
+++ src/compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 llc -march=x86 t.ll -o t.s
 sleep 5
-gcc -m32 -g t.s jlstdlib.cpp -o a.out
+g++ -m32 -g t.s jlstdlib.cpp -o a.out
--- tests/jlstdlib.cpp
+++ tests/jlstdlib.cpp
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include <stdlib.h>
 
-char* itoa(int i, char* s, int dummy_radix) 
+extern "C" char* itoa(int i, char* s, int dummy_radix) 
 {
   sprintf(s, "%d", i);
   return s;
