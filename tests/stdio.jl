06c47f0b547ba5c0faede65b82fcc69756da3b15
James Riley Wynne III <wynnejr@ornl.gov>
2015-05-15 17:18:18 -0400

    Adding a vector addition test. Does not work

--- tests/stdio.jl
+++ tests/stdio.jl
@@ -1,9 +1,9 @@
 extern func putchar(char putcharCharacter) -> int;
-extern func printf(string fmt, int a) -> int;
+extern func printf(string fmt, double a) -> int;
 extern func puts(string str) -> int;
 extern func puts(char str) -> int;
 extern func sprintf(char^ str, string format, int i) -> int;
 extern func abs(int n) -> int;
-extern func malloc(int a) -> int^;
-extern func sin(int a) -> int;
-extern func sin(int a) -> int;
+extern func malloc(int a) -> double^;
+extern func sin(int a) -> double;
+extern func cos(int a) -> double;
--- tests/vecAdd.jl
+++ tests/vecAdd.jl
@@ -1,10 +1,28 @@
 import stdio;
 
-global int globalA = 199
-
 func main() -> int
 {
-  int a = globalA
-  printf("%d ", a)
+  int n = 100000
+  double^ a = malloc(8*n)
+  double^ b = malloc(8*n)
+  double^ c = malloc(8*n)
+  int i
+  int val = n-1
+  for i=0,val {
+    a[i] = sin(i) * sin(i)
+    b[i] = cos(i) * cos(i)
+  }
+
+  for i=0,val {
+    c[i] = a[i] + b[i]
+  }
+  double sum = 0
+  for i=0,val {
+    sum = sum + c[i]
+  }
+
+  sum = sum/n
+
+  printf("Final result: %f ",sum)
   1
 }
